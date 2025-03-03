0f521e56768cea0b6e8fd0bd887554355c138d7b
HyperBlitzer <josearreguin1676@gmail.com>
2025-03-02 21:48:11 -0800

    Update astro.config.mjs

--- tutorial/astro.config.mjs
+++ tutorial/astro.config.mjs
@@ -2,4 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    base: '/tutorial', // Only if your site is on a subpath
+  });
